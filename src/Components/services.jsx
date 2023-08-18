fafc1ec75295f363a5df04e71646d83b707826e4
roshan kc <47151288+roshankc123@users.noreply.github.com>
2023-08-18 22:19:17 +0545

    content shortified

--- src/Components/services.jsx
+++ src/Components/services.jsx
@@ -4,42 +4,42 @@ import '../css/services.css';
 const servicesData = [
   {
     title: 'Machine Learning and Data Science',
-    description: 'Harness the power of data with advanced machine learning techniques.',
+    description: 'Machine learning model development and deployment with Data analysis, feature engineering, and predictive modeling',
     image: 'https://img.freepik.com/free-vector/brain-with-digital-circuit-programmer-with-laptop-machine-learning-artificial-intelligence-digital-brain-artificial-thinking-process-concept-vector-isolated-illustration_335657-2246.jpg?w=2000',
   },
   {
-    title: 'Android Development',
-    description: 'Create innovative and user-friendly Android applications.',
+    title: 'Android and IOS Development',
+    description: 'Custom mobile app development for iOS and Android with cross-platform solutions for diverse business needs',
     image: 'https://img.freepik.com/free-vector/app-development-illustration_52683-47931.jpg?w=2000',
   },
   {
     title: 'Web Development',
-    description: 'Build responsive and dynamic websites for your business.',
+    description: 'Responsive website design and development like E-commerce, portfolio, and informational site creation',
     image: 'https://img.freepik.com/free-vector/java-developer-smartphone-software-javascript-coding-writing-application-css-programming-html-source-code-tampering-mobile-program-vector-isolated-concept-metaphor-illustration_335657-1994.jpg?w=2000',
   },
   {
     title: 'UI/UX and Graphics Design',
-    description: 'Design visually appealing user interfaces and stunning graphics.',
+    description: 'User-centered UI/UX design for apps and websites with graphic design, branding, and visual identity solutions',
     image: 'https://img.freepik.com/free-vector/flat-design-ui-ux-background_23-2149093996.jpg',
   },
   {
     title: 'Automation and IoT Services',
-    description: 'Automate processes and leverage IoT technology for efficiency.',
+    description: 'IoT device integration, control, and monitoring with process automation and smart system implementation',
     image: 'https://img.freepik.com/free-vector/internet-things-iot-smart-connection-control-device-network-industry-resident-anywhere-anytime-anybody-any-business-with-internet-it-technology-futuristic-world_1150-61200.jpg?w=2000',
   },
   {
     title: 'Data Analytics and Insights',
-    description: 'Unlock the potential of your business data with our data analytics services. We help you make informed decisions by collecting, analyzing, and visualizing data from various sources. Gain valuable insights and optimize your strategies for growth.',
+    description: 'Data collection, processing, and visualization with insightful dashboards and interactive data reports',
     image: 'https://img.freepik.com/free-vector/big-isolated-employee-working-office-workplace-flat-illustration_1150-41780.jpg',
   },
   {
     title: 'Low Code development using AI',
-    description: 'we believe this is a new stack of app development. Using AI, we create a quick and self-scalable software depending on the requirements. We believe that by utilizing this technology, our clients will be able to maintain and upgrade their systems on their own.',
+    description: 'Rapid AI application development using low-code platforms using AI-driven process enhancements and task automation',
     image: 'https://img.freepik.com/free-vector/hand-drawn-rpa-illustration_23-2149243331.jpg',
   },
   {
     title: 'E-commerce Solutions',
-    description: 'Offer services to set up and customize e-commerce platforms, integrating payment gateways, product catalogs, and inventory management systems.',
+    description: 'End-to-end e-commerce platform setup and customization with payment gateway integration and online store optimization',
     image: 'https://img.freepik.com/free-vector/maternity-care-products-abstract-concept-vector-illustration-maternity-special-products-healthy-natural-cosmetics-clean-care-goods-pregnant-newborn-skin-treatment-abstract-metaphor_335657-4100.jpg',
   },
   
