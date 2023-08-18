01840836e4e206acb802a9f9c91b4b7f0b2cc554
Bibek Chalise <modernbib@gmail.com>
2023-08-18 08:50:08 +0545

    optimized for mobile

--- src/Components/navbar.jsx
+++ src/Components/navbar.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/navbar.css';
-import logo from '../Images/image.png'
+import logo from '../Images/image.png';
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <div className="logo"> <a href="#"><img src={logo} alt="" srcset="" /></a> </div>
-      <ul className="nav-links">
-        <li><a href="#about">About Us</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact Us</a></li>
+      <div className="logo">
+        <a href="#">
+          <img src={logo} alt="" />
+        </a>
+      </div>
+      <button className={`menu-toggle ${isMobileMenuOpen ? 'active' : ''}`} onClick={toggleMobileMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <ul className={`nav-links ${isMobileMenuOpen ? 'active' : ''}`}>
+        <li>
+          <a href="#about">About Us</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Us</a>
+        </li>
       </ul>
     </nav>
   );
